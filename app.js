71e0537ce1494406de56435e4e3aa08046e76227
Cahao L. Vien <cahao.vien001@protonmail.com>
2019-10-19 21:59:21 +0100

    created skeleton of a interactive list object for js

---
+++ app.js
@@ -0,0 +1,14 @@
+var main = function() {
+    "use strict";
+    $(".comment-input button").on("click", function(e) {
+	var $new_comment = $("<p>");
+	var comment_text = $(".comment-input input").val();
+	$new_comment.text(comment_text);
+	$(".comments").append($new_comment);
+	$(".comment-input input").val("");
+	//console.log("Hello World!");
+	//$(".comments").append("<p>this is a new comment</p>");
+    });
+};
+
+$(document).ready(main);
