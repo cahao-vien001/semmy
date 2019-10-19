146d3018afc94385cbc693d966af4618736fe237
Cahao L. Vien <cahao.vien001@protonmail.com>
2019-10-19 17:35:13 -0400

    added enter key functionality from s. purewal's tutorial

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var main = function() {
 	//console.log("Hello World!");
 	//$(".comments").append("<p>this is a new comment</p>");
     });
+    $(".comment-input input").on("keypress", function(e) {
+	if(e.keyCode === 13) {
+	    var $new_comment = $("<p>");
+	    var comment_text = $(".comment-input input").val();
+	    $new_comment.text(comment_text);
+	    $(".comments").append($new_comment);
+	    $(".comment-input input").val("");
+	    //console.log("Hello World!");
+	    //$(".comments").append("<p>this is a new comment</p>");
+	}
+    });
 };
 
 $(document).ready(main);
