c0d8ea70954303e1909ef129072c069a4833c0e4
Cahao L. Vien <cahao.vien001@protonmail.com>
2019-10-19 17:54:53 -0400

    added fading in functionality as tut progresses

--- app.js
+++ app.js
@@ -1,23 +1,39 @@
 var main = function() {
     "use strict";
     $(".comment-input button").on("click", function(e) {
-	var $new_comment = $("<p>");
-	var comment_text = $(".comment-input input").val();
-	$new_comment.text(comment_text);
-	$(".comments").append($new_comment);
-	$(".comment-input input").val("");
-	//console.log("Hello World!");
-	//$(".comments").append("<p>this is a new comment</p>");
+	var $new_comment;
+	if($(".comment-input input").val() !== "") {
+	    $new_comment = $("<p>").text($(".comment-input input").val());
+	    $new_comment.hide();
+	    $(".comments").append($new_comment);
+	    $new_comment.fadeIn();
+	    $(".comment-input input").val("");
+	    // var $new_comment = $("<p>");
+	    // var comment_text = $(".comment-input input").val();
+	    // $new_comment.text(comment_text);
+	    // $(".comments").append($new_comment);
+	    // $(".comment-input input").val("");
+	    // //console.log("Hello World!");
+	    // //$(".comments").append("<p>this is a new comment</p>");
+	}
     });
     $(".comment-input input").on("keypress", function(e) {
 	if(e.keyCode === 13) {
-	    var $new_comment = $("<p>");
-	    var comment_text = $(".comment-input input").val();
-	    $new_comment.text(comment_text);
-	    $(".comments").append($new_comment);
-	    $(".comment-input input").val("");
-	    //console.log("Hello World!");
-	    //$(".comments").append("<p>this is a new comment</p>");
+	    var $new_comment;
+	    if($(".comment-input input").val() !== "") {
+		$new_comment = $("<p>").text($(".comment-input input").val());
+		$new_comment.hide();
+		$(".comments").append($new_comment);
+		$new_comment.fadeIn();
+		$(".comment-input input").val("");
+		// var $new_comment = $("<p>");
+		// var comment_text = $(".comment-input input").val();
+		// $new_comment.text(comment_text);
+		// $(".comments").append($new_comment);
+		// $(".comment-input input").val("");
+		// //console.log("Hello World!");
+		// //$(".comments").append("<p>this is a new comment</p>");
+	    }
 	}
     });
 };
